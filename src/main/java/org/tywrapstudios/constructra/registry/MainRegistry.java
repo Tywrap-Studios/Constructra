d60b848852d749099e47e50eaad12d8f4bacf411
TywrapStudios <tywrap.studio@gmail.com>
2025-02-18 19:22:24 +0100

    Removed redundant resourceKey methods
    Took 45 seconds

--- src/main/java/org/tywrapstudios/constructra/registry/MainRegistry.java
+++ src/main/java/org/tywrapstudios/constructra/registry/MainRegistry.java
@@ -6,9 +6,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.util.Identifier;
 import org.tywrapstudios.constructra.Constructra;
-import org.tywrapstudios.constructra.api.resource.Resource;
 import org.tywrapstudios.constructra.command.CaCommandImpl;
 
 import java.util.ArrayList;
@@ -24,19 +22,12 @@ public class MainRegistry {
         CaBlocks.register();
         CaBlockEntities.register();
         CaScreenHandlers.register();
+        CaSounds.register();
         ALL_ITEM_CONVERTIBLE_CONTENT.addAll(CaItems.ITEMS);
         ALL_ITEM_CONVERTIBLE_CONTENT.addAll(CaBlocks.BLOCKS);
         CommandRegistrationCallback.EVENT.register((dispatcher, access, env) -> CaCommandImpl.register(dispatcher, access));
         Resources.register();
         Constructra.LOGGER.debug("ItemConvertible List size: " + ALL_ITEM_CONVERTIBLE_CONTENT.size());
-    }
-
-    protected static RegistryKey<Resource> resourceKey(String s) {
-        return resourceKey(id(s));
-    }
-
-    protected static RegistryKey<Resource> resourceKey(Identifier i) {
-        return RegistryKey.of(CaRegistries.Keys.RESOURCE, i);
     }
 
     protected static RegistryKey<Item> itemKey(String s) {
