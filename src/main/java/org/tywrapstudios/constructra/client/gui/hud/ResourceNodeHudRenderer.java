deeef2fd8eb9d8f86a34070b7cdff77c63e2dbdf
TywrapStudios <tywrap.studio@gmail.com>
2025-02-18 19:17:50 +0100

    Added Custom Tooltip functionality for ResourceNodeHudRenderer
    Took 57 seconds

--- src/main/java/org/tywrapstudios/constructra/client/gui/hud/ResourceNodeHudRenderer.java
+++ src/main/java/org/tywrapstudios/constructra/client/gui/hud/ResourceNodeHudRenderer.java
@@ -1,15 +1,23 @@
 package org.tywrapstudios.constructra.client.gui.hud;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
+import org.tywrapstudios.constructra.Constructra;
 import org.tywrapstudios.constructra.api.resource.ResourceNode;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Environment(EnvType.CLIENT)
 public class ResourceNodeHudRenderer {
+    protected static final Identifier TEXTURE = Constructra.id("science_tooltip");
+
     public void render(DrawContext context, ResourceNode<?> node) {
         MinecraftClient client = MinecraftClient.getInstance();
         int x = context.getScaledWindowWidth() / 2 + 10;
@@ -22,17 +30,10 @@ public class ResourceNodeHudRenderer {
         texts.add(name);
         texts.add(purity);
 
-        Text miningPrompt = Text.translatable("text.constructra.prompt.mining_instruction", client.options.attackKey.getBoundKeyLocalizedText().getString())
-                .formatted(Formatting.GOLD);
+        MutableText prompt = Text.translatable("text.constructra.prompt.mining_instruction", client.options.attackKey.getBoundKeyLocalizedText().getString()).formatted(Formatting.GOLD);
+        if (node.isObstructed()) prompt.append(" ").append(Text.translatable("text.constructra.info.obstructed").formatted(Formatting.DARK_RED));
 
-        if (node.isObstructed()) {
-            miningPrompt = Text.translatable("text.constructra.prompt.mining_instruction", client.options.attackKey.getBoundKeyLocalizedText().getString())
-                    .formatted(Formatting.GOLD)
-                    .append(" ")
-                    .append(Text.translatable("text.constructra.info.obstructed").formatted(Formatting.DARK_RED));
-        }
-
-        context.drawTooltip(client.textRenderer, texts, x, y);
-        context.drawTooltip(client.textRenderer, miningPrompt, x, y + 30);
+        context.drawTooltip(client.textRenderer, texts, x, y, TEXTURE);
+        context.drawTooltip(client.textRenderer, prompt, x, y + 30, TEXTURE);
     }
 }
