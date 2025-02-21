81a84ae751be5fc0439a221e1578c588629fbffc
TywrapStudios <tywrap.studio@gmail.com>
2025-02-21 18:11:13 +0100

    Added simpler unit conversion
    Took 2 hours 5 minutes

--- src/main/java/org/tywrapstudios/constructra/api/math/ruleset/Units.java
+++ src/main/java/org/tywrapstudios/constructra/api/math/ruleset/Units.java
@@ -3,6 +3,7 @@ package org.tywrapstudios.constructra.api.math.ruleset;
 import org.tywrapstudios.constructra.api.math.exception.InvalidCalculationException;
 
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 /**
  * Because the world has differences and people can't make up their mind, different unit systems exist.
@@ -11,7 +12,7 @@ import java.util.function.Function;
 public class Units {
 
     /**
-     * Converts a double from one Measurement to another
+     * Converts a double from one Measurement to another, this method tries to be as accurate as possible.
      * @param d the original number to convert
      * @param from the Measurement that the double is in
      * @param to the Measurement the double should be converted into
@@ -24,19 +25,29 @@ public class Units {
         else return to.conversionFactor.apply(d);
     }
 
+    /**
+     * Converts a double to another measurement, please note that we do not ensure any checks that the conversion should even be done in the first place, so be careful.
+     * @param d the original number to convert
+     * @param to the Measurement the double should be converted into
+     * @return the converted double
+     */
+    public static double convert(double d, Measurement to) {
+        return to.conversionFactor.apply(d);
+    }
+
     public enum Measurement {
         METRES(System.METRIC, d -> d * 3.28084, Type.LENGTH),
         FEET(System.FREEDOM, d -> d * 0.3048, Type.LENGTH),
         LITRES(System.METRIC, d -> d * 0.264172, Type.VOLUME),
-        GALLONS(System.FREEDOM,d -> d * 3.785412, Type.VOLUME),
+        GALLONS(System.FREEDOM, d -> d * 3.785412, Type.VOLUME),
         GRAMMES(System.METRIC, d -> d * 0.035274, Type.MASS),
         OUNCES(System.FREEDOM, d -> d * 28.34952, Type.MASS),;
 
         public final System system;
         public final Type type;
-        public final Function<Double, Double> conversionFactor;
+        public final UnaryOperator<Double> conversionFactor;
 
-        Measurement(System system, Function<Double, Double> conversionFactor, Type type) {
+        Measurement(System system, UnaryOperator<Double> conversionFactor, Type type) {
             this.system = system;
             this.conversionFactor = conversionFactor;
             this.type = type;
